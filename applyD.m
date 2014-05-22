1c8ade5814ab32552caf253149030e30a113a3ec
uzair010 <uzair010@gmail.com>
2014-05-21 19:06:35 -0700

    applyD now does not involve masking

--- applyD.m
+++ applyD.m
@@ -1,11 +1,6 @@
-function out = applyD(gamma, mask, delta, dim)
+function out = applyD(gamma, delta, dim)
 
   nDimsGamma = sum( size(gamma) > 1 );
-
-  ndimsMask = sum( size(mask) > 1 );
-  if(nDimsGamma ~= ndimsMask)
-    error('gamma and mask size mismatch');
-  end
 
   if (dim > nDimsGamma)
     error('Dimension argument is too large')
@@ -16,7 +11,6 @@ function out = applyD(gamma, mask, delta, dim)
     case 1 %Applying D in the z direction
     case 2 %Applying D in the x direction
       gamma = gamma';
-      mask = mask';
     case 3 %Appyling D in the y direction
       error('Applying D in the third dimension is not supported yet')
     otherwise
@@ -25,11 +19,11 @@ function out = applyD(gamma, mask, delta, dim)
 
   switch nDimsGamma
     case 1
-      out = applyD1D(gamma, mask, delta);
+      out = applyD1D(gamma, delta);
     case 2
-      out = applyD2D(gamma, mask, delta);
+      out = applyD2D(gamma, delta);
     case 3
-      out = applyD3D(gamma, mask, delta);
+      out = applyD3D(gamma, delta);
     otherwise
       error('Inproper size of gamma');            
   end
@@ -46,30 +40,26 @@ function out = applyD(gamma, mask, delta, dim)
 
 end
 
-function out = applyD3D(gamma, mask, delta)
+function out = applyD3D(gamma, delta)
   [M N P] = size(gamma);
   out = zeros(M, N, P);
   for i = 1:N
     for j = 1:M
-      out(:, i, j) = applyD1D(gamma(:, i, j), mask(:,i, j), delta);
+      out(:, i, j) = applyD1D(gamma(:, i, j), delta);
     end
   end
 end
 
-function out = applyD2D(gamma, mask, delta)
+function out = applyD2D(gamma, delta)
   [M N] = size(gamma);
   out = zeros(M, N);
   for i = 1:N
-    out(:,i) = applyD1D(gamma(:,i), mask(:,i), delta);
+    out(:,i) = applyD1D(gamma(:,i), delta);
   end
 end
 
-function out  = applyD1D(gamma, mask, delta)
+function out  = applyD1D(gamma, delta)
   out = zeros( numel(gamma), 1 );
   out(1:end-1) = ( gamma(2:end)-gamma(1:end-1) ) ./ delta;
-  shifted = circshift(mask, [-1, 0]);
-	shifted(end,:) = 1;
-  tmp = mask & shifted;
-  out = out .* tmp;
 end
 
