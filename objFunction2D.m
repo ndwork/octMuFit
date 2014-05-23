7ca7cd50cdbc6ba0fa757a66c4a990a952030819
uzair010 <uzair010@gmail.com>
2014-05-22 17:52:25 -0700

    Fixed objFunction2D

--- objFunction2D.m
+++ objFunction2D.m
@@ -5,6 +5,6 @@ function out = objFunction2D(gam,I,mask,dz,dx,z,z0,zR,eta)
   Dgz = applyD(gam, dz, 1);
   Dgx = applyD(gam, dx, 2);
   
-  out = 0.5 * sum ( mask.*(Ag - b).^2 ) + eta*sum(abs(mask.*Dgz)) + eta*sum(abs(mask.*Dgx));
+  out = 0.5 * sum ( mask(:).*(Ag(:) - b(:)).^2 ) + eta*sum(abs(mask(:).*Dgz(:))) + eta*sum(abs(mask(:).*Dgx(:)));
 
 end
