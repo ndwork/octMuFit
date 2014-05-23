a06cd912bc70204934e3aa1182978c07bc53d33e
uzair010 <uzair010@gmail.com>
2014-05-22 20:09:05 -0700

    updated objFunction2D
    The objective function takes in two regularization parameters, one for z
    and one for x

--- objFunction2D.m
+++ objFunction2D.m
@@ -1,10 +1,10 @@
-function out = objFunction2D(gam,I,mask,dz,dx,z,z0,zR,eta)
+function out = objFunction2D(gam,I,mask,dz,dx,z,z0,zR,etaZ, etaX)
   
   b = makeb(I);
   Ag = applyA(gam, I, dz, z, z0, zR);
   Dgz = applyD(gam, dz, 1);
   Dgx = applyD(gam, dx, 2);
   
-  out = 0.5 * sum ( mask(:).*(Ag(:) - b(:)).^2 ) + eta*sum(abs(mask(:).*Dgz(:))) + eta*sum(abs(mask(:).*Dgx(:)));
+  out = 0.5 * sum ( mask(:).*(Ag(:) - b(:)).^2 ) + etaZ*sum(abs(mask(:).*Dgz(:))) + etaX*sum(abs(mask(:).*Dgx(:)));
 
 end
