c3bab709d76d66f78eb359e4fe3c9ca17930fd0e
U-inspiron530S\ndwork <ndwork@inspiron530S.(none)>
2014-10-11 20:34:35 -0700

    Adding vermeerSim to repository.

---
+++ vermeerSim.m
@@ -0,0 +1,21 @@
+
+function vermeerSim
+
+  z0 = -0.5;
+  %noiseProportion = 1d-5;
+  noiseProportion = 0;
+  muValues = [1 3];
+  muThicks = [1];
+
+  [I, z, z0, zR, alpha, beta, L0, maxSig, trueMu] = makePhantom( ...
+    1, z0, noiseProportion, muValues, muThicks);
+
+  muVer = muFitVermeer( I, z );
+
+  plot( z, trueMu, 'k', 'LineWidth', 2 );
+  ylim([0 5]);
+  hold on;
+  plot( z, muVer, 'LineWidth', 2 );
+  legend( 'True Mu', 'Vermeer', 'Location', 'Northwest' );
+
+end
