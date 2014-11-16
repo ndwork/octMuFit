d3c0941bb51e33e736aa7edc93ded692428922a0
U-inspiron530S\ndwork <ndwork@inspiron530S.(none)>
2014-11-16 12:10:43 -0800

    runMuFit2D.m is now cleaner.  Results compared to Faber when imaging intralipids.

--- runMuFit2D.m
+++ runMuFit2D.m
@@ -5,7 +5,7 @@ function runMuFit2D
 
   muAlpha = 0;    % Note, if we know true value than problem is better
 
-  dataCase = 16;
+  dataCase = 13;
   %dataCase = 0;   % Simulation
   [I, z, dx, z0, zR, muAlpha, muBeta, muL0, ALA, trueMu ] = ...
     loadOctData( dataCase, false );
@@ -41,23 +41,32 @@ function runMuFit2D
   %[muFit, diagnostics ]= muFit2D_ADMM(I, mask, z, dx, z0, zR, etaz, etax );
   muFit_mVer = muFit2D_mVer( I, z, z0, zR );
 
-  meanI = mean(I,2);
-  muFit_faber = muFitFaber( meanI, [200], z, z0, zR );
-  muFit_faber2 = muFitFaber( meanI, [200,220,350], z, z0, zR );
-  %Note: for Intralipid 20, need to change the 200 above to 170.
-  plot( muFit_faber, 'r', 'LineWidth', 2 );
-  hold on; plot( muFit_faber2, 'k:', 'LineWidth', 2 );
-  plot( muFit_mVer(:,200), 'b', 'LineWidth', 1 );  ylim([0 10]);
-
-  muFit = muFit_mVer;
+  %muFit = muFit_mVer;
   %muFit = muFit2D_TV( I, z, z0, zR );
-  %muFit = muFit2D_whTV( I, z, z0, zR, mask );
+  muFit = muFit2D_whTV( I, z, z0, zR, mask );
   %muFit = weightedTvDenoise_CP( muFit_mVer, paramsCP );
   %muFit = muFit2D_vReg( I, z, z0, zR, mask );
   %muFit = muFit2D_mVer_gBlur( I, z, z0, zR );
   timeTaken = toc;
   %profile off;
   disp(['Time taken (s):', num2str(timeTaken)]);
+
+  if dataCase >= 11 && dataCase <= 16
+    dz = z(2)-z(1);
+    [~, N] = size(I);
+    midCol = floor(N/2);
+    skinLoc = find( mask(:,midCol)==1, 1 );
+    depth5mm = skinLoc + floor(0.5/dz);
+    meanI = mean(I,2);
+    muFit_faber = muFitFaber( meanI, [skinLoc,depth5mm], z, z0, zR );
+    %plot( muFit_faber, 'r', 'LineWidth', 2 );
+    %hold on;
+    %plot( muFit_mVer(:,midCol), 'b', 'LineWidth', 1 );  ylim([0 5]);
+    meanFaber = mean( muFit_faber(skinLoc:depth5mm) );
+    meanFit = mean( muFit(skinLoc:depth5mm,midCol) );
+    disp(['MuFit Faber: ', num2str( meanFaber ) ]);
+    disp(['MuFit mVer: ', num2str( meanFit ) ]);
+  end
 
   figure, imshow( muFit, [0 5.0] );
 
